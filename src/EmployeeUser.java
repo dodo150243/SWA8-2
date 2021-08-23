eb2d7082d6f8a7b47a3136cf7cf73771247340ab
dodo150243 <tirawat330@gmail.com>
2021-08-23 15:43:46 +0700

    Initial commit

---
+++ src/EmployeeUser.java
@@ -0,0 +1,9 @@
+class EmployeeUser implements IUser {
+	
+	@Override
+	public String getname() {
+		// TODO Auto-generated method stub
+		return"This is Employee User";
+	}
+	
+}
---
+++ src/ManagerUser.java
@@ -0,0 +1,12 @@
+class ManagerUser implements IUser{
+	
+
+	@Override
+	public String getname() {
+		// TODO Auto-generated method stub
+		return "This is Manager User";
+	}
+
+
+	
+}
